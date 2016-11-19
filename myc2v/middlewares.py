8fe6d45262f35c103ed5ef79f1f74f203bb3c62e
yannistannier <tannier.yannis@gmail.com>
2016-11-19 18:17:23 +0100

    final change middleware

--- myc2v/middlewares.py
+++ myc2v/middlewares.py
@@ -9,10 +9,10 @@ class SetLastVisitMiddleware(object):
 
     def process_response(self, request, response):
         url = request.get_full_path()
-        
+        return response
         if not request.is_secure():
             return HttpResponsePermanentRedirect( "https://www.pitchmyjob.com"+url )
-        return response
+        
         if 'pitchmyjob.fr' in request.META['HTTP_HOST']:
             return HttpResponsePermanentRedirect( "https://www.pitchmyjob.com/" )
         
